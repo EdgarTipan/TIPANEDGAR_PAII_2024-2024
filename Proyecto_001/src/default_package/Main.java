d1165bdac34c5fa47296dd8b0bde76bc84aed531
EdgarTipan <evtipanl1@uce.edu.ec>
2024-05-12 23:32:16 -0500

    Proyecto actualizado

---
+++ Proyecto_001/src/default_package/Main.java
@@ -0,0 +1,18 @@
+package default_package;
+
+/**
+ * @author Edgar Tipan
+ * 
+ * 
+ */
+
+public class Main {
+				
+    public static void main(String[] args) {  
+    	new JFrameGL();   	
+            
+    }
+}
+
+
+
